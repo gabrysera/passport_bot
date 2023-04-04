74b1086dad1c7d92a2ad45674c4546eb4877fbc7
gabriele <gabrieleserafini10100@gmail.com>
2023-04-04 12:34:48 +0200

    firefox

--- passaporto.py
+++ passaporto.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
 
 
+def set_options():
+    firefox_options = Options()
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    #firefox_options.add_argument('--headless')
+
+    driver = webdriver.Firefox(options=firefox_options, executable_path=GeckoDriverManager().install())
+    return driver
 
 # Navigate to the website
+
+driver = set_options()
 driver.get("https://prenotami.esteri.it/Services")
+print("hello")
 
